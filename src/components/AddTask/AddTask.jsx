1a4a48b8c22300caef31921abdfd1aee6361a127
Zajith Corro <zajithcorrov@hotmail.com>
2022-07-14 21:55:02 -0500

    :lipstick: [styles] Change the name of the classes of component AddTask

--- src/components/AddTask/AddTask.jsx
+++ src/components/AddTask/AddTask.jsx
@@ -2,17 +2,17 @@ import styles from './AddTask.module.css';
 
 const AddTask = () => {
 	return (
-		<div className={styles.add_task}>
-			<div className={styles.button_add}>
-				<button className={styles.btn}>+</button>
+		<div className={styles.addTask}>
+			<div className={styles.btnAddTaskContainer}>
+				<button className={styles.btnAddTask}>+</button>
 			</div>
-			<div className={styles.input_container}>
+			<div className={styles.inputAddTaskContainer}>
 				<label htmlFor="addt" className="d-none">
 					Create todo
 				</label>
 				<input
 					type="text"
-					className={styles.input}
+					className={styles.inputAddTask}
 					placeholder="Create a new todo..."
 					spellCheck="false"
 					autoComplete="off"
