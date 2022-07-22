e3dc1250c291079f3b1dc2a6ae2d47486001a132
Zajith Corro <zajithcorrov@hotmail.com>
2022-07-21 23:47:11 -0500

    :lipstick: [styles] Add a class when the task is completed

--- src/components/TaskList/TaskItem.jsx
+++ src/components/TaskList/TaskItem.jsx
@@ -1,13 +1,15 @@
 import styles from './TaskItem.module.css';
 
 const TaskItem = ({ message, completed }) => {
+	const classCompleted = completed ? styles.taskCompleted : '';
+
 	return (
 		<div className={styles.taskItem}>
 			<div className={styles.checkBoxContainer}>
 				<input className={styles.checkBox} type="checkbox" />
 				<span className={styles.checkBoxSpan} />
 			</div>
-			<p className={styles.taskItemMessage}>{message}</p>
+			<p className={`${styles.taskItemMessage} ${classCompleted}`}>{message}</p>
 			<button className={styles.btnClear}>
 				<img src="src/assets/icon-cross.svg" alt="Clear it" />
 			</button>
