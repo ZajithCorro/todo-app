bfa47e8395d80a1a8cc70f447e93debe689c16a3
Zajith Corro <zajithcorrov@hotmail.com>
2022-07-25 23:26:59 -0500

    :pencil2: [fix] Fix an error to load the images

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,11 +1,12 @@
 import classes from './header.module.css';
+import IconSun from '../../assets/icon-sun.svg';
 
 const Header = () => {
 	return (
 		<header className={classes.card}>
 			<h1>TODO</h1>
 			<button id="theme-switcher">
-				<img src="/src/assets/icon-sun.svg" alt="Change color theme" />
+				<img src={IconSun} alt="Change color theme" />
 			</button>
 		</header>
 	);
--- src/components/TaskList/TaskItem.jsx
+++ src/components/TaskList/TaskItem.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 
 import styles from './TaskItem.module.css';
+import IconCross from '../../assets/icon-cross.svg';
 
 const TaskItem = ({ id, message, completed, removeTodo, completeTodo }) => {
 	const [isCompleted, setIsCompleted] = useState(completed);
@@ -25,7 +26,7 @@ const TaskItem = ({ id, message, completed, removeTodo, completeTodo }) => {
 			</div>
 			<p className={`${styles.taskItemMessage} ${classCompleted}`}>{message}</p>
 			<button className={styles.btnClear} onClick={() => removeTodo(id)}>
-				<img src="src/assets/icon-cross.svg" alt="Clear it" />
+				<img src={IconCross} alt="Clear it" />
 			</button>
 		</li>
 	);
