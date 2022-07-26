651d2e81a7484c7ca394d91fbb1332721a086fa3
Zajith Corro <zajithcorrov@hotmail.com>
2022-07-25 21:53:29 -0500

    :sparkles: [feat] Logic to create new todo. Delete initial array of todos

--- src/components/AddTask/AddTask.jsx
+++ src/components/AddTask/AddTask.jsx
@@ -1,8 +1,25 @@
+import { useState } from 'react';
+import { v4 as uuid } from 'uuid';
+
 import styles from './AddTask.module.css';
 
-const AddTask = () => {
+const AddTask = ({ addTodo }) => {
+	const [todo, setTodo] = useState('');
+
+	const handleSubmit = (e) => {
+		e.preventDefault();
+		const newTodo = {
+			message: todo,
+			completed: false,
+			id: uuid(),
+		};
+
+		addTodo(newTodo);
+		setTodo('');
+	};
+
 	return (
-		<div className={styles.addTask}>
+		<form className={styles.addTask} onSubmit={handleSubmit}>
 			<div className={styles.btnAddTaskContainer}>
 				<button className={styles.btnAddTask}>+</button>
 			</div>
@@ -17,9 +34,11 @@ const AddTask = () => {
 					spellCheck="false"
 					autoComplete="off"
 					id="addt"
+					value={todo}
+					onChange={(e) => setTodo(e.target.value)}
 				/>
 			</div>
-		</div>
+		</form>
 	);
 };
 
--- src/components/App/index.jsx
+++ src/components/App/index.jsx
@@ -1,4 +1,4 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 
 import Header from '../Header';
 import AddTask from '../AddTask';
@@ -7,22 +7,9 @@ import Stats from '../Stats/Stats';
 
 import BackGroundLight from '../../assets/bg-desktop-light.jpg';
 
-const todos = [
-	{
-		message: 'Learn React',
-		completed: false,
-	},
-	{
-		message: 'Learn Redux',
-		completed: false,
-	},
-	{
-		message: 'Learn React Native',
-		completed: false,
-	},
-];
-
 function App() {
+	const [todos, setTodos] = useState([]);
+
 	useEffect(() => {
 		document.documentElement.style.setProperty(
 			'--img-bg',
@@ -30,11 +17,15 @@ function App() {
 		);
 	}, []);
 
+	const addTodo = (newTodo) => {
+		setTodos([...todos, newTodo]);
+	};
+
 	return (
 		<>
 			<Header />
 			<main>
-				<AddTask />
+				<AddTask addTodo={addTodo} />
 				<TaskList todos={todos} />
 				<Stats />
 			</main>
