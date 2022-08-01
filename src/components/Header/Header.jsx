fa9e3cc9abd37ec52952101dd0ecd5965c759627
Zajith Corro <zajithcorrov@hotmail.com>
2022-07-31 23:07:51 -0500

    :sparkles: [feature] Icon and using the Context
    Logic to render one icon depending on the theme. Event to change the theme

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,12 +1,21 @@
+import { useContext } from 'react';
+
+import { ThemeContext } from '../../context/ThemeContext';
+
 import classes from './header.module.css';
 import IconSun from '../../assets/icon-sun.svg';
+import IconMoon from '../../assets/icon-moon.svg';
 
 const Header = () => {
+	const { darkMode, switchTheme } = useContext(ThemeContext);
+
+	const Icon = darkMode ? IconMoon : IconSun;
+
 	return (
 		<header className={classes.card}>
 			<h1>TODO</h1>
-			<button id="theme-switcher">
-				<img src={IconSun} alt="Change color theme" />
+			<button id="theme-switcher" onClick={switchTheme}>
+				<img src={Icon} alt="Change color theme" />
 			</button>
 		</header>
 	);
